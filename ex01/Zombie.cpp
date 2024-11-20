13d62154fc6fcf48ba6c5f54e3ab82cb75fbd7b0
Bovic101 <Victorbolanle72@gmail.com>
2024-11-20 14:03:27 +0100

    Implemtation of the main program ans source file for ex01

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,29 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 13:08:58 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/20 13:08:59 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:32:50 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string zombieName)
+{
+	this->_name = zombieName;
+	std::cout<< "The name of ex01 Zombie baby is:"<< this->_name <<std::endl;
+}
+Zombie::~Zombie()
+{
+	std::cout << "Destroyed ex01 zombie baby:"<<this->_name <<std::endl;
+}
+
+void	Zombie::announce()
+{
+	std::cout <<this->_name <<" :BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie(void)
+{
+	this->_name="(default)";
+	std::cout << "Unknown zombie born" << this->_name <<std::endl;
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,25 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 13:07:32 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/20 13:07:33 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/20 14:00:23 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+int main()
+{
+	int N = 9; //No. of zombies in thehorde
+	std::string name = "Victor";
+
+	Zombie* horde =zombieHorde(N, name);//creating horde
+	int i =0; //iterate to make zombie announce itself
+
+	while (i < N)
+	{
+		horde[i].announce();
+		++i;
+	}
+	delete[] horde;
+	return(0);
+}
