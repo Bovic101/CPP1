94f6919db76ed3eb683e77d843f6211f88d6d9c7
Bovic101 <Victorbolanle72@gmail.com>
2024-11-20 17:00:00 +0100

    Implementation update  of Weapon

--- ex03/ClassWeapon.cpp
+++ ex03/ClassWeapon.cpp
@@ -6,34 +6,34 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:33:30 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/20 16:14:38 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:43:30 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClassWeapon.hpp"
 
-Weapon::Weapon(std::string type):_type(type)
+Weapon::Weapon(const std::string& type) : _type(type) 
 {
-	std::cout<<"Contructor"<< std::endl;
+    std::cout << "Constructor" << std::endl; 
 }
 
-Weapon::Weapon(void)
+Weapon::Weapon()
 {
-	std::cout << "Defaulte Constructor" << std::endl;
+    std::cout << "Default Constructor" << std::endl;
 }
 
-Weapon ::~Weapon(void)
+Weapon::~Weapon()
 {
-	std::cout <<"Default Destructor" << std::endl;
+    std::cout << "Default Destructor" << std::endl;
 }
 
 const std::string& Weapon::getType() const
 {
-	return(_type);
+    return _type;
 }
 
 void Weapon::setType(const std::string& type)
 {
-	_type= type;
+    _type = type;
 }
 
--- ex03/ClassWeapon.hpp
+++ ex03/ClassWeapon.hpp
@@ -6,7 +6,7 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:34:04 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/20 15:55:14 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:41:44 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 
 class Weapon
 {
-	private:
-	std::string _type;
-	
-	public:
-	Weapon(const std::string type);
-	Weapon(void);
-	~Weapon();
-	
-	const std::string& getType() const;
-	void setType(const std::string& type);
+private:
+    std::string _type;
+
+public:
+    Weapon(const std::string& type);  // constructor name and parameter passing
+    Weapon();                         // Constructor with no parameters
+    ~Weapon();                        // Destructor
+
+    const std::string& getType() const;
+    void setType(const std::string& type);
 };
 
 #endif
