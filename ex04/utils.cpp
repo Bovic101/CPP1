e04203c322c67cd8e6140ea4f6059d60e469cfc6
vodebunm <vodebunm@student.42heilbronn.de>
2024-11-30 01:15:17 +0100

    error fixing in ex04

--- ex04/utils.cpp
+++ ex04/utils.cpp
@@ -6,47 +6,49 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 09:51:10 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/21 10:35:02 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/30 01:07:02 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sed.hpp"
+#include <fstream>
 
-void newFileCreation(const std::string &filename, const std::string &str1,const std::string &str2)
+void newFileCreation(const std::string &filename, const std::string &str1, const std::string &str2)
 {
-	//To open the current file
-	std::ifstream cur_file(filename);
-	
-	if (!cur_file.is_open())
-	{
-		std::cout<<"Eroor:Unable to open file" <<filename <<std::endl;
-		return;
-	}
-	//File where the output is redirected to
-	std::string new_filename = filename + ".replace";
-	std::ofstream new_file(new_filename);
-	//Open new_file for new string
-	if (!new_file.is_open())
-	{
-		std::cout<<"Eroor:Unable to create new file" <<new_filename <<std::endl;
-		cur_file.close();
-		return;
-	}
-	//read each line and str1 with str2
-	std::string line_reader;
-	while (std::getline(cur_file, line_reader))
-	{
-		//Find and replace ste1 with str2
-		size_t i = 0;
-		while (i = line_reader.find(str1, i) != std::string::npos)
-		{
-			line_reader.erase(i,str1.length());
-			line_reader.insert(i, str2);
-			i +=str2.length();
-		}
-		//Write the formatted line to the newfile
-		new_file <<line_reader <<std::endl;
-	}
-	cur_file.close();
-	new_file.close();
+    // Open the current file
+    std::ifstream cur_file(filename);
+    if (!cur_file.is_open())
+    {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return;
+    }
+
+    // File where the output is redirected
+    std::string new_filename = filename + ".replace";
+    std::ofstream new_file(new_filename);
+    if (!new_file.is_open())
+    {
+        std::cerr << "Error: Unable to create new file " << new_filename << std::endl;
+        cur_file.close();
+        return;
+    }
+
+    // Read each line and replace occurrences of str1 with str2
+    std::string line_reader;
+    while (std::getline(cur_file, line_reader))
+    {
+        size_t pos = 0;
+        while ((pos = line_reader.find(str1, pos)) != std::string::npos)  // Corrected line
+        {
+            line_reader.replace(pos, str1.length(), str2);
+            pos += str2.length(); // Move past the replaced part
+        }
+
+        // Write the formatted line to the new file
+        new_file << line_reader << std::endl;
+    }
+
+    // Close the files
+    cur_file.close();
+    new_file.close();
 }
